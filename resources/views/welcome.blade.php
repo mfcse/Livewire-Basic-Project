f6d050dc1484bb0db84283f386edac6d0b2319a0
Maruf <maruf.cu.cse09@gmail.com>
2021-08-30 20:28:48 +0600

    added tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     @livewireStyles
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     
     {{-- @livewire('counter') --}}
     @livewire('comments')
+
+    
     @livewireScripts
+    <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
